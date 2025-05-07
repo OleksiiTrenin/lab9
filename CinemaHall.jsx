bc028c149fe0f86ac2bedef1f576b961cb289608
OleksiiTrenin <OLEKSII.TRENIN@lnu.edu.ua>
2025-05-07 20:37:08 +0300

    сам кінотеатр

--- CinemaHall.jsx
+++ CinemaHall.jsx
@@ -6,3 +6,19 @@ function CinemaHall() {
   const [showtimes] = useState(['16:20', '17:00', '17:40', '19:00']);
   const [selectedShowtime, setSelectedShowtime] = useState('16:20');
   const [seatsByShowtime, setSeatsByShowtime] = useState({});
+
+ const generateSeats = () => {
+    return Array(96).fill().map((_, index) => {
+      const row = Math.floor(index / 12) + 1;
+      const isSuperLux = row >= 7;
+      const isBooked = Math.random() < 0.3;
+      return {
+        id: index + 1,
+        row,
+        booked: isBooked,
+        selected: false,
+        type: isSuperLux ? 'SUPER LUX' : 'GOOD',
+        price: isSuperLux ? 270 : 160,
+      };
+    });
+  };
