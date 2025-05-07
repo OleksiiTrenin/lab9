571e0e7342011ac774f73b4b76468773065a28b7
OleksiiTrenin <OLEKSII.TRENIN@lnu.edu.ua>
2025-05-07 20:33:55 +0300

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,2 +1,11 @@
 import React from 'react';
 import MovieList from '../components/MovieList';
+function Home({ movies, searchTerm }) {
+  return (
+    <div>
+      <MovieList movies={movies} searchTerm={searchTerm} />
+    </div>
+  );
+}
+
+export default Home;
