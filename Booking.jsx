c697a4ecc4b0d94216f1328a6629bf53836532a2
OleksiiTrenin <OLEKSII.TRENIN@lnu.edu.ua>
2025-05-07 20:32:32 +0300

    додано вибір місіць

--- Booking.jsx
+++ Booking.jsx
@@ -3,3 +3,16 @@ import { useParams } from 'react-router-dom';
 import CinemaHall from '../components/CinemaHall';
 import './Booking.css';
 
+
+function Booking() {
+  const { id } = useParams();
+
+  return (
+    <div className="booking-container">
+      <h2>Вибір місць - Сеанс {id}</h2>
+      <CinemaHall />
+    </div>
+  );
+}
+
+export default Booking;
