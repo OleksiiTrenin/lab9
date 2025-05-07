45d486b9ff2d0e873f4f0229377afe5489ae3249
OleksiiTrenin <OLEKSII.TRENIN@lnu.edu.ua>
2025-05-07 20:36:41 +0300

    оголошення станів

--- CinemaHall.jsx
+++ CinemaHall.jsx
@@ -1,2 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './CinemaHall.css';
+function CinemaHall() {
+  const [seats, setSeats] = useState([]);
+  const [selectedSeatsByShowtime, setSelectedSeatsByShowtime] = useState({}); 
+  const [showtimes] = useState(['16:20', '17:00', '17:40', '19:00']);
+  const [selectedShowtime, setSelectedShowtime] = useState('16:20');
+  const [seatsByShowtime, setSeatsByShowtime] = useState({});
